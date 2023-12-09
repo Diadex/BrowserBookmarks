51fa6af0a07a5966efd0d9743a293ac4bd0f5d2e
Ali Emir Güzey <aliemirguzey@gmail.com>
2023-12-09 23:00:00 +0300

    Add a functional address bar

---
+++ src/renderer/AddressBar.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { ReactComponent as GoButton } from '../../assets/icons/go.svg';
+
+interface AddressBarProps {
+  onGoClick: (url: string) => void;
+}
+
+function AddressBar({ onGoClick }: AddressBarProps) {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleGoClick = () => {
+    onGoClick(inputValue);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      onGoClick(inputValue);
+    }
+  };
+
+  return (
+    <div className="AddressBar">
+      <input
+        type="text"
+        placeholder="Enter URL"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={handleKeyPress} // Add this line to handle key press
+      />
+      <button onClick={handleGoClick}>
+        <GoButton width="20" height="20" />
+      </button>
+    </div>
+  );
+}
+
+export default AddressBar;
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,39 +1,39 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import MenuBar from './MenuBar';
 
 function Hello() {
+  const [urlText, setUrlText] = useState('https://www.bing.com');
+  const [iframeWidth, setIframeWidth] = useState(window.innerWidth);
+  const [iframeHeight, setIframeHeight] = useState(window.innerHeight - 40);
+
+  const handleResize = () => {
+    setIframeWidth(window.innerWidth);
+    setIframeHeight(window.innerHeight - 40);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleGoClick = (url: string) => {
+    setUrlText(url);
+  };
+
   return (
     <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+      <MenuBar onGoClick={handleGoClick} />
+      <div className="Web">
+        <iframe
+          src={urlText}
+          width={iframeWidth}
+          height={iframeHeight}
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
