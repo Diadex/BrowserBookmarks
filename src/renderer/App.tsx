37a931f0bdbaccdc326a357295e36582cf74df92
Alperen Yal <alperenutku2002@gmail.com>
2023-12-12 00:19:52 +0300

    added fixes, added changes, now mostly working tabs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
 
@@ -10,14 +16,33 @@ interface TabProps {
 }
 
 function Tab({ to, label, onRemove }: TabProps) {
+  const location = useLocation();
+  const isOpen = location.pathname === to;
   return (
     <Link to={to} className="link">
-      <div className="tab">
-        <text className="tab_writing">{label}</text>
-        <button onClick={onRemove} type="button" className="tab_close">
-          X
-        </button>
-      </div>
+      {isOpen ? (
+        <div className="tab_open">
+          <div className="tab_format">
+            <span className="tab_writing">{label}</span>
+            <div style={{ width: 50 }}>
+              <button onClick={onRemove} type="button" className="tab_close">
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="tab">
+          <div className="tab_format">
+            <span className="tab_writing">{label}</span>
+            <div style={{ width: 50 }}>
+              <button onClick={onRemove} type="button" className="tab_close">
+                X
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </Link>
   );
 }
@@ -69,7 +94,6 @@ export default function App() {
   const [tabs, setTabs] = useState([
     { id: generateRandomId(), label: 'Tab 1', to: '/' },
   ]);
-
   const addTab = () => {
     const newTab = {
       id: generateRandomId(),
@@ -78,16 +102,39 @@ export default function App() {
     };
     setTabs([...tabs, newTab]);
   };
-
   const removeTab = (id: string) => {
+    const indexToRemove = tabs.findIndex((tab) => tab.id === id);
+    let nextTabIndex;
+    if (indexToRemove > 0) {
+      // If the tab to be removed is not the first one,
+      // select the tab before it.
+      nextTabIndex = indexToRemove - 1;
+    } else if (indexToRemove === 0 && tabs.length > 1) {
+      // If the first tab is being closed and there are more tabs,
+      // select the tab after it.
+      nextTabIndex = 1;
+    } else {
+      // If the closing tab is the only one, or the last one is being closed,
+      // there won't be any tabs left to display.
+      nextTabIndex = null;
+    }
     const updatedTabs = tabs.filter((tab) => tab.id !== id);
     setTabs(updatedTabs);
+    if (nextTabIndex !== null) {
+      // Use the selected tab index to update the URL.
+      const nextTab = tabs[nextTabIndex];
+      // How do I switch to this path?
+      window.location.hash = `#${nextTab.to}`;
+    }
   };
 
   return (
     <Router>
       <div className="All">
         <div className="tabs">
+          <button onClick={addTab} type="button" className="tab_add">
+            +
+          </button>
           {tabs.map((tab) => (
             <Tab
               key={tab.id}
@@ -96,9 +143,6 @@ export default function App() {
               onRemove={() => removeTab(tab.id)}
             />
           ))}
-          <button onClick={addTab} type="button" className="tab_add">
-            +
-          </button>
         </div>
         <Routes>
           {tabs.map((tab) => (
