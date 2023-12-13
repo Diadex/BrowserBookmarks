7208f8224e1e66ddd1851b1057667a24e4738cae
Ali Emir Güzey <hey@guezey.me>
2023-12-13 20:36:14 +0300

    fixx

--- src/main/main.ts
+++ src/main/main.ts
@@ -545,13 +545,13 @@ const createWindow = async () => {
     height: 728,
     icon: getAssetPath('icon.png'),
     webPreferences: {
-      nodeIntegration: true,
       webviewTag: true,
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
     },
   });
+
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
   mainWindow.on('ready-to-show', () => {
